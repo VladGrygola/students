eae265b47bcb9b2b39f33fe75e9fd388fe8230ac
Vlad <44097600+VladGrygola@users.noreply.github.com>
2019-01-17 17:27:38 +0200

    Add footer

--- html/localhost/www/index.php
+++ html/localhost/www/index.php
@@ -7,7 +7,9 @@
     <link rel="stylesheet" type="text/css" href="css/main.css" />
 </head>
 <body>
-    
+    <footer>
+        Made by Vlad Grygola
+    </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="js/main.js"></script>
 </body>
