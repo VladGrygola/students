4890e7e4b826383aa6c5cd5904caacc98842a64d
Vlad <44097600+VladGrygola@users.noreply.github.com>
2019-01-17 17:22:02 +0200

    Include 'Montserrat' font

--- html/localhost/www/index.php
+++ html/localhost/www/index.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="utf-8" />
     <title>Students Database</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,600,700" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/main.css" />
 </head>
 <body>
-
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="js/main.js"></script>
 </body>
