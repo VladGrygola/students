3f2bd72e0c2c3674a7910dcf704a74fc2a3fd2b0
Vlad <44097600+VladGrygola@users.noreply.github.com>
2019-01-17 17:13:55 +0200

    Include css, js(jquery) in index.php

--- html/localhost/www/index.php
+++ html/localhost/www/index.php
@@ -1,3 +1,13 @@
-<?php 
-    echo "Test";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Students Database</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+</head>
+<body>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
