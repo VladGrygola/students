79afeecfd397b2c7329c22436d3d211f2d593b44
Vlad <44097600+VladGrygola@users.noreply.github.com>
2019-01-18 14:18:44 +0200

    Create php file-library for work with tables and SQL

---
+++ html/localhost/www/libSql.php
@@ -0,0 +1,44 @@
+<?php
+	class Table
+	{
+		static public function getTable($table_name, $db_name = "students")
+		{
+			$mysqli = new mysqli("localhost", "root", "password_0", $db_name);
+			// Check connection
+			if ($mysqli->connect_errno) {
+			    printf("Connect failed: %s\n", $mysqli->connect_error);
+			    exit();
+			}
+			$query = "SELECT * FROM " . $db_name . "." . $table_name;
+			// Display number of rows
+			if ($result = $mysqli->query($query)) {
+			    printf("Кількість записів: %d\n", $result->num_rows);
+			}
+			return $result;
+		}
+		static public function showTable($table)
+		{
+			echo "<table>";
+			$first = true; 
+			while($row = $table->fetch_assoc()) {
+				if ($first) { 
+					//  Display column names
+					foreach ($row as $key => $value) {
+					    echo "<th>";
+					    echo $key;
+					    echo "</th>";
+					}
+					$first = false;
+				}
+				echo "<tr>";
+		        foreach ($row as $key => $value) {
+		        	// Display rows
+		        	echo "<td>";
+		        	echo $row[$key];
+		        	echo "</td>";
+		        }
+		        echo "</tr>";
+			}
+			echo "</table>";
+		}
+	}
